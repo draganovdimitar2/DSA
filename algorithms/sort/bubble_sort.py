b3bb69a9e46a57971104fd0caf31aacbdd3af72a
draganovdimitar2 <draganovdimitar2@gmail.com>
2025-04-14 16:55:56 +0300

    Initial commit: added bubble sort

---
+++ algorithms/sort/bubble_sort.py
@@ -0,0 +1,10 @@
+def bubble_sort(arr):
+    for i in range(len(arr) - 1):
+        for j in range(len(arr) - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+
+    return arr
+
+
+print(bubble_sort([5, 8, 2, 4, 6, 7, 2]))
